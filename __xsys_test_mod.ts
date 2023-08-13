c1be7c1b9c1fc2834fb01743cba8ea52e5ed741a
Eric L. Bentley <11730553+ebntly@users.noreply.github.com>
2023-08-12 22:44:13 -0500

    test: Add unique exports for cross-package testing.

---
+++ __xsys_test_mod.ts
@@ -0,0 +1,9 @@
+/**
+ * This file exports select features used for testing in other packages.
+ *
+ * @copyright 2022 integer11. All rights reserved. MIT license.
+ */
+
+export { I11N_EX2_HOST, I11N_EX2_PATH } from './src/_internals/constants.ts';
+
+export { allCodesFixture } from './tests/fixtures/unique_codes.fixtures.ts';
